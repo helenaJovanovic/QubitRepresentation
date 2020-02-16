85590d02448c6e823e6e4099da95abfd3dd67eae
helenaJovanovic <41912173+helenaJovanovic@users.noreply.github.com>
2020-02-16 14:11:38 +0100

    Izmene naziva i poboljsanje izgleda strelica

--- QubitRepresentation.py
+++ QubitRepresentation.py
@@ -7,73 +7,48 @@ from tkinter.ttk import *
 import threading
 from tkinter import messagebox
 
-""" 
-    Od biblioteka uvozimo:
-
-    1. numpy - koji sluzi za matematicka izracunavanja i za predstavljanje odredjenih tipova koje inace ne bi imali u pajtonu,
-    kao sto su numpy.complex koji se koristi ovde i float64 koji je superiorniji float tip nego onaj sa kojim pajton dolazi jer
-    moze da sadrzi vise decimala. U kodu koristimo brojne matematcike funkcije kao sto su npr. numpy.random.choice i cojugate
-
-    2. matplotlib - i njene podklase koje sluze za efikasno grafiranje funkcija, objekata i statistickih prikaza bilo u 2d ili 3d.
-    matplot.pyplot predstavlja klasu koja sadrzi podatke o povrsini/prostoru na koju grafiramo i funkcije za prikaz na povrsini
-    mpl_toolkits.mplot3d.axes3d je podklasa kojom implementiramo 3d objekte i u njoj imamo nekoliko predefinisanih tipova specificnih za 3d
-    
-    3. tkinter - je klasa za pravljenje GUI prikaza programa koja dolazi upakovana uz Pajton. Ova klasa izgleda malo zastarelo pa smo
-    uveli i tkinter.ttk da bi modernizovali izgled prema danasnjim standardima
-
-    4. threading - zbog cinjenice da otvaramo vise prozora u ovom programu i da zelimo da svaki funkcionise u isto vreme a ne da
-    glavni ceka izvrsavanje prozora koji je pokrenuo (I samim tim se zaustavi) dodali smo jednu komponentu multiprogramiranja 
-    i implementirali niti. Svaki otvoren prozor ima svoju nit
-
-    Program smo implementirali koristeci objektno orijentisano programiranje zbog intuitivnosti i bolje citljivosti
-    koda s obzirom na njegov obim.
-"""
 
 
 class PROGRAM:
     def __init__(self):
-        """
-            U konsuktorskoj funkciji pravimo GUI
-
-        """
         
-        self.root = Tk() #Poziv za pokretanje tkintera, self.root predstavlja ime glavnog prozora
+        self.root = Tk() 
 
-        self.AlfaR = DoubleVar()#Promenjive koje sadrze ono sto je upisano u Entry
-        self.AlfaI = DoubleVar()#Imaju svoj tip koji je definisan preko tkinter
+        #Promenjive koje sadrze ono sto je upisano u Entry
+        self.AlfaR = DoubleVar()
+        self.AlfaI = DoubleVar()
         self.BetaR = DoubleVar()
         self.BetaI = DoubleVar()
-        
-        self.root.geometry("500x300")#rezolucija i naziv prozora
-        self.root.title("Simulacija kjubita")
 
-        label1 = Label(text = "Alfa (Realni deo): ")#Label koji sadrzi tekst
-        label1.place(x=50, y=50)#.place funkcija koja postavlja objekat na (x, y) tacku na prozoru
+        #rezolucija i naziv prozora
+        self.root.geometry("500x300")
+        self.root.title("Qubit Representation")
 
-        self.AlfaREntry = Entry(textvar=self.AlfaR, width=5)#Element za upis podataka. U textvar definisemo promenljivu koja sadrzi podatke iz Entry
+        #Ucitavamo vrednosti :
+        label1 = Label(text = "Alpha (Real part): ")
+        label1.place(x=50, y=50)
+        self.AlfaREntry = Entry(textvar=self.AlfaR, width=5)
         self.AlfaREntry.place(x=180, y=50)
-
-        #Sve dalje je isto
-
-        label2 = Label(text = "(Imaginarni deo): ")
+ 
+        label2 = Label(text = "(Imaginary part): ")
         label2.place(x=250, y=50)
         self.AlfaIEntry = Entry(textvar=self.AlfaI, width=5)
         self.AlfaIEntry.place(x=380, y=50)
 
-        label3 = Label(text = "Beta (Realni deo): ")
+        label3 = Label(text = "Beta (Real part): ")
         label3.place(x=50, y = 150)
         self.BetaREntry = Entry(textvar=self.BetaR, width=5)
         self.BetaREntry.place(x=180, y = 150)
     
-        label4 = Label(text = "(Imaginarni deo): ")
+        label4 = Label(text = "(Imaginary part): ")
         label4.place(x=250, y = 150)
         self.BetaIEntry = Entry(textvar=self.BetaI, width=5)
         self.BetaIEntry.place(x=380, y = 150)
 
-        self.button = Button(text="Pokreni bloh", command=self.Bloh) #Dugme koje ako prisinemo idemo na self.Bloh i pozivamo prikaz Blohove sfere
+        self.button = Button(text="Bloch's sphere", command=self.Bloh) #Dugme koje ako prisinemo idemo na self.Bloh i pozivamo prikaz Blohove sfere
         self.button.place(x=100, y=220)
 
-        self.button = Button(text="Izracunaj", command=self.Izracunaj) #Dugme, command je funkcija koja se poziva ako se pritisne.
+        self.button = Button(text="Calculate", command=self.Izracunaj) #Dugme, command je funkcija koja se poziva ako se pritisne.
         self.button.place(x=325, y=220)
         #Kada pritisnemo izracunaj idemo na funkciju self.Izracunaj
 
@@ -103,9 +78,8 @@ class PROGRAM:
         self.x = np.sin(Theta)*np.cos(Phi)
         self.y = np.sin(Theta)*np.sin(Phi)
         self.z = np.cos(Theta)
+        
         #Ovde unapred racunam tacku na Blohovoj sferi na koju vektor tog kjubita pokazuje
-
-
         normala = np.sqrt((self.alfa*np.conjugate(self.alfa) + self.beta*np.conjugate(self.beta)))
         #Formula sqrt(a^2 + b^2)
 
@@ -117,13 +91,13 @@ class PROGRAM:
 
     def Bloh(self):
 
-        if(self.uslov()):#Proveravam uslov u odvojenoj funkciji koja vraca True ili False
-            threading.Thread(target=self.pokreniBloh).start()#I pokrecem odvojenu nit i u njoj prozor koji ce prikazivati Blohovu sferu
+        if(self.uslov()):
+            threading.Thread(target=self.pokreniBloh).start()   #Pokrecem odvojenu nit i u njoj prozor koji ce prikazivati Blohovu sferu
         else:
-            messagebox.showerror("Greska", "Nije kjubit") #Inace ako nije kjubit iskace prozor za greske
+            messagebox.showerror("Greska", "Nije kjubit")       #Inace ako nije kjubit iskace prozor za greske
     
     def pokreniBloh(self):
-        u = np.linspace(0, np.pi, 15)#Definisemo vektore koji su specificni za sferu
+        u = np.linspace(0, np.pi, 15)                           #Definisemo vektore koji su specificni za sferu
         v = np.linspace(0, 2 * np.pi, 15)
 
 
@@ -133,34 +107,35 @@ class PROGRAM:
         z = np.outer(np.cos(u), np.ones_like(v))#np.ones_like -> Return an array of ones with the same shape and type as a given array.
 
         fig = plt.figure() #Konstruisemo prozor i prostor za prikaz
-        fig.canvas.set_window_title('Blohova sfera')#ime
-        ax = plt.axes(projection='3d')#Tip projekcije
+        fig.canvas.set_window_title('Blohova sfera')
+        ax = plt.axes(projection='3d')
 
-        ax.set_axis_off()#Iskljucujem default ose
+        ax.set_axis_off()
 
-        ax.text(0, 0, 1.4, '|0>', fontsize=14)#Malo teskta na ovim koordinatama (0, 0, +-1.4)
-        ax.text(0, 0, -1.4, '|1>', fontsize=14) #Teksta sa vrha i donjeg dela sfere
+        ax.text(0, 0, 1.4, '|0>', fontsize=14)
+        ax.text(0, 0, -1.4, '|1>', fontsize=14) 
 
-        ax.quiver(0, 0, 0, np.float64(1.0), 0, 0, length = 1, normalize = True, color="green")
-        ax.quiver(0, 0, 0, 0, np.float64(1.0), 0, length = 1, normalize = True, color="red")
-        ax.quiver(0, 0, 0, 0, 0, np.float64(1.0),length = 1, normalize = True, color ="blue")
-        #Ose koje pravim kao vektore prva tri broja predstavljaju pocetnu tacku a sledecih tri predstavlja krajnju tacku vektora
-        #normalizujem ih i dajem im boje
+        ###KOORDINATE###
+        ax.quiver(0, 0, 0, np.float64(1.0), 0, 0, arrow_length_ratio = 0.1, length = 1, normalize = True, color="green")
+        ax.quiver(0, 0, 0, 0, np.float64(1.0), 0, arrow_length_ratio = 0.1, length = 1, normalize = True, color="red")
+        ax.quiver(0, 0, 0, 0, 0, np.float64(1.0), arrow_length_ratio = 0.1, length = 1, normalize = True, color ="blue")
+        
 
-
-        ax.quiver(0, 0, 0, self.x, self.y, self.z, length = 1, normalize = True, color="black")
         #Vektor koji predstavlja kjubit u sferi i pokazuje na tacku izracunatu u self.uslov()
+        
+        ax.quiver(0, 0, 0, self.x, self.y, self.z, arrow_length_ratio = 0.1,length = 1, normalize = True, color="black")
+        
 
-        ax.plot_wireframe(x, y, z, color="grey", linewidth=0.5)#Pravimo sferu kao wireframe, tj kao kostur da se tako izrazim
-        #Onda mozemo da vidimo unutar nje
+        ax.plot_wireframe(x, y, z, color="grey", linewidth=0.5)#Pravimo sferu kao wireframe, tj kao kostur
+        
         plt.show()#I prikazujemo napravljen prikaz/prostor
 
     def Izracunaj(self):
-        #Proveravamo uslov za kjubit stanje u if pozivom funkcije self.uslov
+        
         if(self.uslov()):
             threading.Thread(target=self.pokreniIzracunaj).start() #Pokrecemo funkciju unutar nove niti i tad se poziva prozor
         else:
-            messagebox.showerror("Greska", "Nije kjubit") #Inace iskace prozor sa greskom
+            messagebox.showerror("Greska", "Nije kjubit")           #Inace iskace prozor sa greskom
 
     def pokreniIzracunaj(self):
         fig = plt.figure(figsize=(8, 4)) #Pokrecemo prozor na kojem cemo da prikazemo prostor i podesavamo velicinu prozora
@@ -169,13 +144,13 @@ class PROGRAM:
         ax.set_axis_off()#Uklanjam ose koje se nalaze sa strane jer mi ne trebaju za ovaj prikaz
         ax.dist = 30#I posmatram prikaz sa daljine od 30
 
-        ax.quiver(0, 0, 0, np.float64(4.0), 0, 0, length = 1, color="green")
+        ax.quiver(0, 0, 0, np.float64(4.0), 0, 0, length = 1, color="green", arrow_length_ratio = 0.05)
         #quiver predstavlja vektor, gde prva tri broja predstavljaju pocetnu tacku a sledeca tri zavrsnu
         #Prva tri quiver-a su x, y i z ose
-        ax.quiver(0, 0, 0, 0, np.float64(4.0), 0, length = 1, color="red")
-        ax.quiver(0, 0, 0, 0, 0, np.float64(4.0),length = 1, color ="blue")
+        ax.quiver(0, 0, 0, 0, np.float64(4.0), 0, length = 1, color="red", arrow_length_ratio = 0.05)
+        ax.quiver(0, 0, 0, 0, 0, np.float64(4.0),length = 0.5, color ="blue", arrow_length_ratio = 0.05)
        
-        ax.quiver(0, 0, 0, 0, 0, 1, length=1, normalize = True, color="black")
+        ax.quiver(0, 0, 0, 0, 0, 1, length=1, normalize = True, color="black", arrow_length_ratio = 0.1)
         #Vektor koji predstavlja ilustraicju kjubita koji dolazi u dodir sa operatorom merenja (pre merenja)
 
 
@@ -224,9 +199,9 @@ class PROGRAM:
         #Stavljamo tekst na prostor prema kojem ce vektor pokazivati u zavisnosti od merenja
 
         if(result==1):
-            ax.quiver(0, 0, np.float64(1), np.float64(1), 0, np.float64(.5), color="red") #Vektor ka |1> ako je izmeren 1
+            ax.quiver(0, 0, np.float64(1), np.float64(1), 0, np.float64(.5), color="red", arrow_length_ratio = 0.1) #Vektor ka |1> ako je izmeren 1
         else:
-            ax.quiver(0, 0, np.float64(1), np.float64(-1), 0, np.float64(.5), color="red")#Vektor ka |0> ako je izmeren 0
+            ax.quiver(0, 0, np.float64(1), np.float64(-1), 0, np.float64(.5), color="red", arrow_length_ratio = 0.1)#Vektor ka |0> ako je izmeren 0
 
         ax.text(0, 1.2, 1, 'M0 ', fontsize=14)#Naziv pored pravougaonika koji predstavlja operator merenja
         ax.text(-1, 1.9, 1, 'P0 = ' + str(np.float64(p0)), fontsize=14) #Rezultat racunanja p0
